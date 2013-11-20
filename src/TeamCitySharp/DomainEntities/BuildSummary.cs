ce2ba227224c5bb0a6384d84b0c27188efb7dde0
Greg Walker <none@none>
2013-11-19 18:54:20 -0600

    More properties on BuildSummary

--- src/TeamCitySharp/DomainEntities/BuildSummary.cs
+++ src/TeamCitySharp/DomainEntities/BuildSummary.cs
@@ -6,13 +6,19 @@ namespace TeamCitySharp.DomainEntities
     {
         public string Id { get; set; }
         public string Number { get; set; }
-        public bool Running { get; set; }
-        public int PercentageComplete { get; set; }
         public string Status { get; set; }
         public string BuildTypeId { get; set; }
+        public DateTime StartDate { get; set; }
         public string Href { get; set; }
         public string WebUrl { get; set; }
-        public DateTime StartDate { get; set; }
+
+        //When: locator=running:True
+        public bool Running { get; set; }
+        public int PercentageComplete { get; set; }
+
+        //Not certain when these show up
+        public string BranchName { get; set; }
+        public bool? DefaultBranch { get; set; }
 
         public override string ToString()
         {
