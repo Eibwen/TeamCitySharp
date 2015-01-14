b0092c48327ed18418796e2d56de704df2c008ed
Eibwen <rejk0rz+gh@gmail.com>
2015-01-14 13:42:14 -0600

    Build and BuildSummary from TeamCity 8.1.5

--- src/TeamCitySharp/DomainEntities/Build.cs
+++ src/TeamCitySharp/DomainEntities/Build.cs
@@ -5,27 +5,42 @@ namespace TeamCitySharp.DomainEntities
     public class Build
     {
         public string Id { get; set; }
+        public string BuildTypeId { get; set; }
         public string Number { get; set; }
+        //TODO this could be an enum
         public string Status { get; set; }
+        //TODO this could be an enum
+        public string State { get; set; }
+
+        public bool Running { get; set; }
+        public int PercentageComplete { get; set; }
+
         public string Href { get; set; }
         public string WebUrl { get; set; }
-        public bool Personal { get; set; }
-        public bool History { get; set; }
-        public bool Pinned { get; set; }
-        public bool Running { get; set; }
-        public RunningInfo RunningInfo { get; set; }
+
+        //  These do not exist in guestAuth anyway...
+        ////public bool Personal { get; set; }
+        ////public bool History { get; set; }
+        ////public bool Pinned { get; set; }
         public string StatusText { get; set; }
         public BuildConfig BuildType { get; set; }
+        public object Tags { get; set; }
+        public RunningInfo RunningInfo { get; set; }
+
+        public DateTime QueuedDate { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime? FinishDate { get; set; }
-        public Agent Agent { get; set; }
-        public object Tags { get; set; }
-        public Properties Properties { get; set; }
-        public object SnapshotDependencies { get; set; }
-        public object ArtifactDependencies { get; set; }
-        public object Revisions { get; set; }
+
+        //TODO Create objects for all these commented ones
         //public Triggered Triggered { get; set; }
+        //public LastChanges LastChanges { get; set; }
         public ChangeWrapper Changes { get; set; }
+        //public Revisions Revisions { get; set; }
+        public Agent Agent { get; set; }
+        //public Artifacts Artifacts { get; set; }
+        //public RelatedIssues RelatedIssues { get; set; }
+        public Properties Properties { get; set; }
+        //public Statistics Statistics { get; set; }
 
         public override string ToString()
         {
--- src/TeamCitySharp/DomainEntities/BuildSummary.cs
+++ src/TeamCitySharp/DomainEntities/BuildSummary.cs
@@ -5,20 +5,19 @@ namespace TeamCitySharp.DomainEntities
     public class BuildSummary
     {
         public string Id { get; set; }
-        public string Number { get; set; }
-        public string Status { get; set; }
         public string BuildTypeId { get; set; }
-        public DateTime StartDate { get; set; }
+        public string Number { get; set; }
+        //TODO this could be an enum
+        public string Status { get; set; }
+        //TODO this could be an enum
+        public string State { get; set; }
+
         public string Href { get; set; }
         public string WebUrl { get; set; }
 
         //When: locator=running:True
         public bool Running { get; set; }
         public int PercentageComplete { get; set; }
-
-        //Not certain when these show up
-        public string BranchName { get; set; }
-        public bool? DefaultBranch { get; set; }
 
         public override string ToString()
         {
